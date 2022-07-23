1cffd357bf6971594c5eee77f1a807e98107069e
SpicyWasab <98230093+SpicyWasab@users.noreply.github.com>
2022-07-23 19:38:13 +0200

    Fully working drawer.
    :)

--- src/lib/Drawer.svelte
+++ src/lib/Drawer.svelte
@@ -16,10 +16,17 @@
     import Drawer, { Content, Scrim, Header, Title, Subtitle } from '@smui/drawer';
     import List, { Item, Text, Separator, Graphic } from '@smui/list';
     
+    import { navigating } from '$app/stores'; 
+    import { goto } from '$app/navigation';
     import { databaseStarted } from './state.mjs';
-    
+
     let currentActivity = undefined;
     let disabled = false;
+
+    $: if($navigating?.to != undefined) {
+        closeDrawer(); // if navigating while drawer is opened, then we should probably close it :)
+        currentActivity = $navigating.to.pathname;
+    }
 
     $: if($databaseStarted) {
         disabled = false;
@@ -35,11 +42,11 @@
     </Header>
     <Content>
         <List>
-            <Item activated={currentActivity === 'default:all'} on:click={() => { }}>
+            <Item activated={currentActivity === '/actives'} on:click={() => goto('/actives')}>
                 <Graphic class="material-icons">checklist</Graphic>
                 <Text>Actifs</Text>
             </Item>
-            <Item activated={currentActivity === 'default:archive'} on:click={() => { }}>
+            <Item activated={currentActivity === '/archive'} on:click={() => goto('/archive')}>
                 <Graphic class="material-icons">archive</Graphic>
                 <Text>Archivés</Text>
             </Item>
