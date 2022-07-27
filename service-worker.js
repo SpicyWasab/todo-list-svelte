1779de323f50805fb1797ac5c79c33dd20b5ed98
SpicyWasab <98230093+SpicyWasab@users.noreply.github.com>
2022-07-27 11:49:56 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,18 @@
 const n = [
-  "/todo-list-svelte/_app/immutable/start-204f6a8f.js",
-  "/todo-list-svelte/_app/immutable/pages/__layout.svelte-b08fd73a.js",
+  "/todo-list-svelte/_app/immutable/start-0c520bc9.js",
+  "/todo-list-svelte/_app/immutable/pages/__layout.svelte-f01e3384.js",
   "/todo-list-svelte/_app/immutable/pages/__error.svelte-9a357fdd.js",
   "/todo-list-svelte/_app/immutable/pages/categories/_...unknownCategory_.svelte-5c88dd3b.js",
   "/todo-list-svelte/_app/immutable/pages/categories/_category_category_/lists/_...unknownList_.svelte-1d2103c1.js",
-  "/todo-list-svelte/_app/immutable/pages/categories/_category_category_/lists/_list_.svelte-f02aed9e.js",
-  "/todo-list-svelte/_app/immutable/pages/categories/_category_category_.svelte-219a1008.js",
+  "/todo-list-svelte/_app/immutable/pages/categories/_category_category_/lists/_list_.svelte-588a4ffe.js",
+  "/todo-list-svelte/_app/immutable/pages/categories/_category_category_.svelte-c6049d39.js",
   "/todo-list-svelte/_app/immutable/assets/[category=category]-750f9fef.css",
   "/todo-list-svelte/_app/immutable/pages/index.svelte-bc024c2f.js",
   "/todo-list-svelte/_app/immutable/chunks/index-f7c1fb44.js",
   "/todo-list-svelte/_app/immutable/chunks/singletons-3371444f.js",
   "/todo-list-svelte/_app/immutable/chunks/paths-b9644fda.js",
   "/todo-list-svelte/_app/immutable/chunks/db-07f01c9f.js",
-  "/todo-list-svelte/_app/immutable/chunks/Drawer-31d0a32b.js",
+  "/todo-list-svelte/_app/immutable/chunks/Drawer-d1948221.js",
   "/todo-list-svelte/_app/immutable/chunks/navigation-da1e81ae.js",
   "/todo-list-svelte/_app/immutable/chunks/Fab-01ceb877.js"
 ], p = [
