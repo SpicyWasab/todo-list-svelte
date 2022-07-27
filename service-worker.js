4b657c8aa707bc46e4549c491dc23b2d2c9cee36
SpicyWasab <98230093+SpicyWasab@users.noreply.github.com>
2022-07-27 03:18:10 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const t = [
-  "/todo-list-svelte/_app/immutable/start-e38fbad3.js",
+  "/todo-list-svelte/_app/immutable/start-2ee591a5.js",
   "/todo-list-svelte/_app/immutable/pages/__layout.svelte-b08fd73a.js",
   "/todo-list-svelte/_app/immutable/pages/__error.svelte-9a357fdd.js",
   "/todo-list-svelte/_app/immutable/pages/categories/_...unknownCategory_.svelte-5c88dd3b.js",
